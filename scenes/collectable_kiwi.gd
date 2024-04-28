e4c62ce0caedb051131cd30a2aae4bc19aef3163
Gemerson Ribas <gmribas1@gmail.com>
2024-04-28 12:06:35 -0300

    fix collectables animations

--- scenes/collectable_kiwi.gd
+++ scenes/collectable_kiwi.gd
@@ -2,12 +2,21 @@ extends Area2D
 
 @onready var game_manager = %GameManager
 @onready var kiwi_2d = get_node("Kiwi2D")
-@onready var collected_2d = get_node("Collected2D")
+
+const STATE_DEFAULT = 0
+const STATE_COLLECTED = 1
+const DEFAULT_ANIMATION = "default"
+const COLLECTED_ANIMATION = "collected"
+
+const animations = [DEFAULT_ANIMATION, COLLECTED_ANIMATION]
+var currentState = STATE_DEFAULT
 
 func _on_body_entered(body):
-	if (body.name == "VirtualGuy"):
+	if (body.name == "VirtualGuy" && kiwi_2d.animation == DEFAULT_ANIMATION):
+		currentState = STATE_COLLECTED
 		game_manager.add_points()
-		#kiwi_2d.queue_free()
-		kiwi_2d.visible = false
-		collected_2d.visible = true
-		#queue_free()
+		kiwi_2d.animation = animations[currentState]
+
+func _on_kiwi_2d_animation_looped():
+	if currentState == STATE_COLLECTED:
+		queue_free()
