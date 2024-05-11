27679dcc28e5384d54cc7f0cdf52d35eba9cd5f5
Gemerson Ribas <gmribas1@gmail.com>
2024-05-11 13:55:51 -0300

    character jumping state using projectile arc

--- scripts/characters/state/jumping_state.gd
+++ scripts/characters/state/jumping_state.gd
@@ -4,12 +4,23 @@ extends CharacterState
 
 const ANIMATION = "jumping"
 
+@export var jump_height: float
+@export var jump_time_to_peak: float
+@export var jump_time_to_descent: float
+
+@onready var jump_velocity: float = (2.0 * jump_height) / jump_time_to_peak
+@onready var jump_gravity: float = (2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)
+@onready var fall_gravity: float= (2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)
+
 @onready var sprite_2d = $"../../Sprite2D"
 @onready var player_attributes = $"../../../../PlayerAttributes"
 @onready var game_manager = $"../../../../GameManager"
+	
+func get_gravity() -> float: 
+	return jump_gravity if character.velocity.y < 0.0 else fall_gravity
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func jump(delta):
+	character.velocity.y = jump_velocity
 	
 func physics_update(delta):	
 	#End game early return
@@ -18,11 +29,11 @@ func physics_update(delta):
 		
 	# Add the gravity.
 	if not character.is_on_floor():
-		character.velocity.y += gravity * delta
+		character.velocity.y += get_gravity() * delta
 		sprite_2d.animation = ANIMATION
 	else:
 		state_machine.transition_to("MovementCharacterState")
 		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and character.is_on_floor():
-		character.velocity.y = player_attributes.JUMP_VELOCITY
+		jump(delta)
--- scripts/player_attributes.gd
+++ scripts/player_attributes.gd
@@ -2,5 +2,4 @@ extends Node
 
 const SPEED = 150.0
 const DECELERATION = 10	
-const JUMP_VELOCITY = -900.0
 const DEFAULT_LIFE = 5
